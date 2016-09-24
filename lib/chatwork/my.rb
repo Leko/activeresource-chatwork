a2aba4b00e987f6fe5c4db390131471680d52728
Leko <leko.noor@gmail.com>
2016-09-25 00:25:02 +0900

    feat: Add my API and tests

---
+++ lib/chatwork/my.rb
@@ -0,0 +1,14 @@
+
+module Chatwork
+  class My < Base
+    self.collection_name = 'my'
+
+    def self.status
+      get(:status)
+    end
+
+    def self.tasks(params = {})
+      get(:tasks, params).map {|t| Chatwork::Task.new(t, true)}
+    end
+  end
+end
--- lib/chatwork/task.rb
+++ lib/chatwork/task.rb
@@ -3,6 +3,9 @@ module Chatwork
   class Task < Base
     include Chatwork::NestOfRoom
 
+    STATUS_OPEN = 'open'
+    STATUS_DONE = 'done'
+
     self.prefix = '/v1/rooms/:room_id/'
     self.collection_name = 'tasks'
     self.primary_key = 'task_id'
