a6196f096556a25ccd67bf7277f79339bac35ba4
Leko <leko.noor@gmail.com>
2016-09-25 00:12:42 +0900

    fix: Invalid test case name

--- spec/chatwork/contact_spec.rb
+++ spec/chatwork/contact_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Chatwork::Contact do
-  it 'can list contacts according to chatroom' do
+  it 'can list my contacts' do
     contacts = Chatwork::Contact.all
     expect(contacts.all? { |c| c.is_a? Chatwork::Contact }).to be true
   end
