49fe67fdd324335788c1af7f3485b69610f4c189
Shingo Inoue <inoue.shingo@hamee.co.jp>
2016-09-24 23:06:00 +0900

    feat: Add member API and tests

--- spec/chatwork/member_spec.rb
+++ spec/chatwork/member_spec.rb
@@ -1,6 +1,16 @@
 require 'spec_helper'
 
 describe Chatwork::Member do
-  it 'can list members according to chatroom'
-  it 'can update member according to chatroom'
+  it 'can list members according to chatroom' do
+    params = {params: {room_id: ENV['CHATWORK_MYCHAT_ID']}}
+    members = Chatwork::Member.all(params)
+    expect(members.all? {|m| m.is_a? Chatwork::Member}).to be true
+  end
+
+  it 'can retrieve belongs to room' do
+    params = {params: {room_id: ENV['CHATWORK_MYCHAT_ID']}}
+    member = Chatwork::Member.first(params)
+
+    expect(member.room).to be_instance_of Chatwork::Room
+  end
 end
