a0b70fad62651a32a074add97922fc228b9b9eff
Leko <leko.noor@gmail.com>
2016-09-25 00:49:11 +0900

    chore: Add codecov settings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'chatwork'
 require 'dotenv'
 require 'simplecov'
+require 'codecov'
 
 Dotenv.load
 
@@ -9,6 +10,7 @@ SimpleCov.start do
   add_filter '/vendor/'
   add_filter '/spec/'
 end
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
 
 # テスト用にロガーを差し込む
 module Chatwork
