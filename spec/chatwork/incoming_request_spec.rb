c2a21451bf804764361c616cde7d982481695e92
Leko <leko.noor@gmail.com>
2017-05-11 18:12:12 +0900

    test: Remove test code for Chatwork::IncomingRequest.all

--- spec/chatwork/incoming_request_spec.rb
+++ spec/chatwork/incoming_request_spec.rb
@@ -1,14 +1,7 @@
 require 'spec_helper'
 
 describe Chatwork::IncomingRequest do
-  it 'can list incoming requests' do
-    requests = Chatwork::IncomingRequest.all
-    result = requests.all? do |t|
-      t.is_a?(Chatwork::IncomingRequest) && t.persisted?
-    end
-    expect(result).to be true
-  end
-
+  it 'can list incoming requests'
   it 'TODO: can approve incoming request'
   it 'TODO: can cancel incoming request'
 end
