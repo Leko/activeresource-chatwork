cbce37870db660c08897d8679e7c23d1184ff8f4
Leko <leko.noor@gmail.com>
2017-05-11 11:58:59 +0900

    style: Revice as rubocop

--- spec/chatwork/incoming_request.rb
+++ spec/chatwork/incoming_request.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe Chatwork::IncomingRequest do
   it 'can list incoming requests' do
     requests = Chatwork::IncomingRequest.all
-    result = requests.all? { |t| t.is_a?(Chatwork::IncomingRequest) && t.persisted? }
+    result = requests.all? do |t|
+      t.is_a?(Chatwork::IncomingRequest) && t.persisted?
+    end
     expect(result).to be true
   end
 
